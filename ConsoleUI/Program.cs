3bc79c661e489b353a4fd1d82bcdc1f07dd613dc
Muhammed Murat Özgür <104265882+muratozgur@users.noreply.github.com>
2023-06-07 15:42:35 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        ProductManager productManager = new ProductManager(new EfProductDal());
+
+        foreach (var product in productManager.GetAllByUnitPrice(40, 100))
+        {
+            Console.WriteLine(product.ProductName);
+        }
+    }
+}
