f8a7b4908c6ba82fee2fd03ef9f13badbbd45d72
Muhammed Murat Özgür <104265882+muratozgur@users.noreply.github.com>
2023-06-18 18:37:11 +0300

    Result and DataResult objects have been created.

---
+++ Core/Utilities/Results/IResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    //Starting point for the fundemental voids
+    public interface IResult
+    {
+        bool Success { get; }
+        String Message { get; }
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public Result(bool success, string message) : this(success)//if this constructor begins, automaticly other constructor begins too
+        {
+            //Message is readonly-we have only getter-. However, we can set readonly properties in constructors
+            Message = message;
+        }//Overloading
+
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
